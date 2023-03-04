32f8ae1b79781c78c99d90ca27e695abb7937fac
nfel <nfilsaraee@gmail.com>
2023-03-04 17:51:08 +0330

    ~

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	bench "rpc-stats/Benchmark"
 	"rpc-stats/core"
@@ -48,7 +49,7 @@ func TestMC(t *testing.T) {
 		t,
 		runOnAllRPCs(bench.Benchmark.UpdateMulticallStats, func(b bench.Benchmark, err error) {
 			if err != nil {
-				t.Error(err)
+				logrus.Error(err)
 			} else {
 				fmt.Printf("<MC> QT1 %d QT10 %d QT100 %d QT1000 %d QT2000 %d => [%s] \n", b.MS.QT1, b.MS.QT10, b.MS.QT100, b.MS.QT1000, b.MS.QT2000, b.Provider.RPC)
 			}
